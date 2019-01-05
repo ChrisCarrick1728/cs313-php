ce281b9ad2c48c81ebb01ea923f0253d64a3923e
Chris Carrick-2 <car17059@byui.edu>
2019-01-05 13:43:47 -0700

    fixed js issue

--- web/js/hello.js
+++ web/js/hello.js
@@ -3,7 +3,7 @@ window.onload = function () {
    var ajax = new XMLHttpRequest();
    ajax.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-         document.getElementsByTagName(h1)[0].innerHTML = this.responseText;
+         document.getElementsByTagName("h1")[0].innerHTML = this.responseText;
       }
    };
    ajax.open("GET", "php/hello.php", true);
