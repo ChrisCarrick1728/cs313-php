268254da7e366764575970bfc91f360bca3c7ef4
Chris Carrick-2 <car17059@byui.edu>
2019-01-05 13:38:24 -0700

    updated hello world app

---
+++ web/js/hello.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+   // Ajax request
+   var ajax = new XMLHttpRequest();
+   ajax.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         document.getElementsByTagName(h1)[0].innerHTML = this.responseText;
+      }
+   };
+   ajax.open("GET", "php/hello.php", true);
+   ajax.send();
+}
